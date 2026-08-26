fed82150ccab992364933d36d97a15673c811288
salman-JFS <salmankhanbad690@gmail.com>
2026-08-26 23:32:08 +0530

    Add Notification API

---
+++ Notification-API/Notification-API/src/test/java/com/example/Notification_API/NotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notification_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
