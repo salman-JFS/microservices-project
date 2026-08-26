f739e7f20798020a88e126db8d925f84f1c8ee73
salman-JFS <salmankhanbad690@gmail.com>
2026-08-27 00:15:44 +0530

    Add Admin Server

---
+++ Admin-Server/Admin-Server/src/test/java/com/example/Admin_Server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Admin_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
