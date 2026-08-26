b65e5f6b16c7b06afde90e2aeb675e5ebb261db0
salman-JFS <salmankhanbad690@gmail.com>
2026-08-27 00:13:11 +0530

    Add Server Registry

---
+++ Server-Registry/Server-Registry/src/main/java/com/example/Server_Registry/ServerRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.example.Server_Registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class ServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRegistryApplication.class, args);
+	}
+
+}
